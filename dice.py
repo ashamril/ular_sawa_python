6f8b32e92ec648004cf6edc9663b674f89e26842
ashamril <42052498+ashamril@users.noreply.github.com>
2020-07-05 09:06:20 +0800

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,9 @@
+import random
+
+number1 = random.randint(1, 6)
+print("Your 1st dice roll is " + str(number1))
+
+number2 = random.randint(1, 6)
+print("Your 2nd dice roll is " + str(number2))
+
+print("Total is " + str(number1 + number2))
